fb5090e57409e19c8ca65df65190d17e5689e8ec
Liam Magee <liam.magee@gmail.com>
2019-08-15 23:02:15 +1000

    Revise charts

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Simple analysis of DSP and Newstart figures, 2014 - 2018.
 
 Data taken from [DSS Payment Demographic Data](https://data.gov.au/data/dataset/dss-payment-demographic-data).
 
-Indicative output:
+DSP, Newstart and Newstart with Partial Capacity to Work:
 
-![Disability Support Pension and Newstart Figures](https://raw.githubusercontent.com/liammagee/dss-analysis/master/chart.png "Disability Support Pension and Newstart Figures")
+![Disability Support Pension and Newstart Figures](https://raw.githubusercontent.com/liammagee/dss-analysis/master/chart1.png "Disability Support Pension and Newstart Figures")
 
+
+Changes to DSP and Newstart with Partial Capacity to Work:
+
+![Disability Support Pension and Newstart Figures](https://raw.githubusercontent.com/liammagee/dss-analysis/master/chart1.png "Disability Support Pension and Newstart Figures")
--- chart.R
+++ chart.R
@@ -47,31 +47,51 @@ df <- addPeriod(df, 'june-2014-dss-demographics-suppressed-2014-final-ftb.xlsx',
 #df <- addPeriod(df, 'dssdemographicsseptember2013.xlsx', 'Dec13', 2013, 12, sheetTotal = 2, sheetPartial = 14)
 #df <- addPeriod(df, 'dssdemographicsseptember2013.xlsx', 'Sep13', 2013, 9, sheetTotal = 2, sheetPartial = 14)
 
-df$Diff.DSP <- df$Total.DSP - df[length(df$Total.DSP),'Total.DSP']
+df$Diff.DSP <- df$Total.DSP - df[1,'Total.DSP']
+df$Diff.Total.NS <- df$Total.NS - df[length(df$Total.NS),'Total.NS']
 df$Diff.Partial.NS <- df$Partial.NS - df[length(df$Partial.NS),'Partial.NS']
 
-df$Diff.of.Diffs <- df$Diff.Partial.NS - df$Diff.DSP
- 
-dfm <- melt(df, id.vars = c("Date"), measure.vars = c('Total.DSP', 'Total.NS', 'Partial.NS', 'Diff.of.Diffs'))
+
+dfm <- melt(df, id.vars = c("Date"), measure.vars = c('Total.DSP', 'Total.NS', 'Partial.NS'))
 dfm$strdate <- strftime(dfm$Date, "%b %y")
 
+dfm2 <- melt(df, id.vars = c("Date"), measure.vars = c('Diff.DSP', 'Diff.Partial.NS'))
+dfm2$strdate <- strftime(dfm2$Date, "%b %y")
 
-p<-ggplot(data=dfm, aes(x=reorder(strdate, Date), y=value / 1000)) +
-  geom_bar(stat="identity", aes(fill=variable), width = 0.5, position=position_dodge(width = 0.8)) +
+p1 <- ggplot(data=dfm, aes(x=reorder(strdate, Date), y=value / 1000)) +
+  geom_bar(stat="identity", aes(fill=variable), width = 0.6, position=position_dodge(width = 0.7)) +
   theme_bw()  +
   scale_x_discrete(breaks = dfm$strdate, labels = dfm$strdate) +
-  scale_y_continuous(breaks = c(0, 200, 400, 600, 800), labels = paste(c(0, 200, 400, 600, 800), 'K', sep='')) +
+  scale_y_continuous(breaks = c(0, 200, 400, 600, 800), 
+                     labels = paste(c(0, 200, 400, 600, 800), 'K', sep='')) +
   scale_fill_brewer(palette="Dark2", name = "", labels = c("Disability Support Pension", 
                                             "Newstart recipients", 
-                                            "Newstart recipients with partial capacity to work", 
-                                            "Shift from DSP to Newstart, with partial capacity to work")) +
-  labs(x = "", y="") +
+                                            "Newstart recipients with partial capacity to work")) +
+  labs(title = "DSP, Newstart and Newstart with Partial Capacity to Work", subtitle = "June 2014 to December 2018. Source: DSS Payment Demographic Data.", x = "", y="") +
   theme(
     legend.position="bottom",
     legend.direction = "vertical",
         axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.75),
         axis.text.y = element_text())
 
-p
+p1
+ggsave("chart1.png", p1)
 
-ggsave("chart.png")
+p2<-ggplot(data=dfm2, aes(x=reorder(strdate, Date), y=value / 1000)) +
+  geom_bar(stat="identity", aes(fill=variable), width = 0.5, position=position_dodge(width = 0.8)) +
+  theme_bw()  +
+  scale_x_discrete(breaks = dfm2$strdate, labels = dfm2$strdate) +
+  scale_y_continuous(breaks = c(0, 20, 40, 60, 80), 
+                     labels = paste(c(0, 20, 40, 60, 80), 'K', sep='')) +
+  scale_fill_brewer(palette="Dark2", name = "", labels = c("Change in Disability Support Pension", 
+                                                           "Change in Newstart recipients with partial capacity to work")) +
+  labs(title = "Changes from Lowpoint", subtitle = "(DSP lowpoint = Dec 18; Newstart with partial capacity to work lowpoint = Jun 14)", x = "", y="") +
+  theme(
+    legend.position="bottom",
+    legend.direction = "vertical",
+    axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.75),
+    axis.text.y = element_text())
+
+p2
+
+ggsave("chart2.png", p2)
